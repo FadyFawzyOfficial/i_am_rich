bab430b39230ebb1bbc6cedda29daeb303289206
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-01 14:15:01 +0200

    2. Scaffold a Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 // The main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://www.w3schools.com/css/img_lights.jpg'),
+          ),
+        ),
       ),
     ),
   );
