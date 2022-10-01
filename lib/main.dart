7b29e77816470a5fc3d6ea67d6810ce74291c396
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-01 12:38:46 +0200

    1. Create a Flutter project from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+// The main function is the starting point for all our Flutter apps.
 void main() {
-  runApp(const MaterialApp());
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
 }
